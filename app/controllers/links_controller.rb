1d94f275d96817044bf15abb25b772388f447c35
Murray Williams <murray@waterfalldigital.co.za>
2022-02-23 20:28:36 +0200

    Created url shortener functionality

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,12 +1,17 @@
 class LinksController < ApplicationController
+
   def new
   end
 
   def create
+
     @link = Link.new(link_params)
+    @link.slug = SecureRandom.base64(8)
+
+    @bitesize_url = "http://127.0.0.1:3000/" + @link.slug
 
     if @link.save
-      redirect_to new_link_path(url: @link.long_url, slug: @link.slug)
+      redirect_to new_link_path(short_url: @bitesize_url)
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :links, only: [:new, :create]
+  get 'links/new/:slug', to: 'links#show', as: 'short_link'
 end
