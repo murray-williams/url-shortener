ac460ed9f1814fc3c851d84d2fe1541625a0e0f9
Murray Williams <murray@waterfalldigital.co.za>
2022-02-25 09:32:45 +0200

    Allow external links to redirect to original url

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,4 @@
 class LinksController < ApplicationController
-
   def new
   end
 
@@ -7,15 +6,24 @@ class LinksController < ApplicationController
 
     @link = Link.new(link_params)
     @link.slug = SecureRandom.base64(8)
+    @long_url = @link.long_url
 
     @bitesize_url = "http://127.0.0.1:3000/" + @link.slug
 
     if @link.save
-      redirect_to new_link_path(short_url: @bitesize_url)
+      redirect_to new_link_path(short_url: @bitesize_url,long_url: @long_url)
     else
       render :new
     end
 
+  end
+
+  def show
+    split_url = request.fullpath.split("/")
+    find_url_matching_slug = Link.where(slug: split_url[-1])
+    redirect_url = find_url_matching_slug.first.long_url
+    
+    redirect_to redirect_url
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :links, only: [:new, :create]
-  get 'links/new/:slug', to: 'links#show', as: 'short_link'
+  get '/:slug', to: 'links#show', as: 'short_link'
+
 end
