031153928262482f10c9e5cd127ec7e8dee51f2f
Murray Williams <murray@waterfalldigital.co.za>
2022-02-25 09:34:29 +0200

    Updated the base url

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -8,7 +8,7 @@ class LinksController < ApplicationController
     @link.slug = SecureRandom.base64(8)
     @long_url = @link.long_url
 
-    @bitesize_url = "http://127.0.0.1:3000/" + @link.slug
+    @bitesize_url = "https://mysterious-beach-71329.herokuapp.com/" + @link.slug
 
     if @link.save
       redirect_to new_link_path(short_url: @bitesize_url,long_url: @long_url)
@@ -22,7 +22,7 @@ class LinksController < ApplicationController
     split_url = request.fullpath.split("/")
     find_url_matching_slug = Link.where(slug: split_url[-1])
     redirect_url = find_url_matching_slug.first.long_url
-    
+
     redirect_to redirect_url
   end
 
